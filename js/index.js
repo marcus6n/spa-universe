4912e896f21b72446676fa78268e2d9233d9549f
marcus6n <marcusdeoliveiraneves@gmail.com>
2024-11-06 15:28:22 -0300

    Refactor router.js and index.js for better code organization and readability

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-import { Router } from "./router.js";
+import { Router } from "./router.js"; // importa a classe Router do arquivo router.js
 
-const router = new Router();
-router.add("/", "/pages/home.html");
-router.add("/the-universe", "/pages/the-universe.html");
-router.add("/explore", "/pages/explore.html");
-router.add(404, "/pages/404.html");
+const router = new Router(); // instancia a classe Router
+router.add("/", "/pages/home.html"); // adiciona a rota / e a pagina home.html
+router.add("/the-universe", "/pages/the-universe.html"); // adiciona a rota /the-universe e a pagina the-universe.html
+router.add("/explore", "/pages/explore.html"); // adiciona a rota /explore e a pagina explore.html
+router.add(404, "/pages/404.html"); // adiciona a rota 404 e a pagina 404.html
 
-router.handle();
+router.handle(); // chama a funcao handle
 
-window.onpopstate = () => router.handle();
-window.route = () => router.route();
+window.onpopstate = () => router.handle(); // adiciona um evento onpopstate ao window que chama a funcao handle
+window.route = () => router.route(); // adiciona uma funcao route ao window que chama a funcao route
--- js/router.js
+++ js/router.js
@@ -1,26 +1,26 @@
 export class Router {
-  routes = {};
+  routes = {}; // dicionario de rotas
 
-  add(routeName, page) {
-    this.routes[routeName] = page;
+  add(routeName, page) { // adiciona uma rota passando o nome e a pagina "page/rota"
+    this.routes[routeName] = page; // adiciona a rota ao dicionario
   }
 
-  route(event) {
-    event = event || window.event;
-    event.preventDefault();
+  route(event) { // funcao que recebe um evento
+    event = event || window.event; // se o evento for nulo, recebe o evento do window
+    event.preventDefault(); // previne o comportamento padrao do evento
 
-    window.history.pushState({}, "", event.target.href);
+    window.history.pushState({}, "", event.target.href); // adiciona um novo estado ao historico do navegador
 
-    this.handle();
+    this.handle(); // chama a funcao handle
   }
 
-  handle() {
-    const { pathname } = window.location;
-    const route = this.routes[pathname] || this.routes[404];
-    fetch(route)
-      .then((data) => data.text())
-      .then((html) => {
-        document.querySelector("#app").innerHTML = html;
+  handle() { // funcao que manipula as rotas
+    const { pathname } = window.location; // pega o pathname da url
+    const route = this.routes[pathname] || this.routes[404]; // pega a rota correspondente ao pathname ou a rota 404
+    fetch(route) // faz uma requisicao a rota
+      .then((data) => data.text()) // pega o texto da requisicao
+      .then((html) => { // pega o html
+        document.querySelector("#app").innerHTML = html; // adiciona o html ao elemento com id app
       });
   }
 }
