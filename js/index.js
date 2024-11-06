91f807bb06040d909c74df354d44b1451201e3d3
marcus6n <marcusdeoliveiraneves@gmail.com>
2024-11-06 15:19:08 -0300

    Refactor header styles and add fixed position
    - Adjust line height for paragraph text in universe.css
    - Update font import URL syntax in global CSS
    - Add fixed position to header in header.css
    - Add import and routing functionality in index.js and router.js
    - Add .gitignore file to exclude unnecessary files and directories
    - Update package.json with lite-server dependency

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import { Router } from "./router.js";
+
+const router = new Router();
+router.add("/", "/pages/home.html");
+router.add("/the-universe", "/pages/the-universe.html");
+router.add("/explore", "/pages/explore.html");
+router.add(404, "/pages/404.html");
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
